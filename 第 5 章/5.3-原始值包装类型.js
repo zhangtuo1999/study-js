e0b377c9bf7b19b40d4ff2a761f8458bb96f31e3
zhangtuo <13120409722@163.com>
2022-05-03 17:58:27 +0800

    5.3

---
+++ 第 5 章/5.3-原始值包装类型.js
@@ -0,0 +1,117 @@
+// let s1 = "string";
+// let s2 = s1.substring(2);
+// console.log(s2);
+
+// let s1 = new String('string')
+// let s2 = s1.substring(2);
+// s1 = null
+
+// let s1 = 'string'
+// s1.foo = 'bar'
+// console.log(s1.foo);
+
+// let obj = new Object('string')
+// console.log(obj instanceof String);
+
+// let val = "25";
+// let num = Number(val);
+// console.log(typeof num);
+
+// let obj = new Number(val);
+// console.log(typeof obj);
+
+// let obj = new Boolean(true);
+// console.log(obj.valueOf());
+// console.log(obj.toString());
+
+// let obj = new Number(10);
+// console.log(obj);
+
+// let num = new Number(10)
+// console.log(num.toString(2));   // 1010
+// console.log(num.toString(8));   // 12
+// console.log(num.toString(16));  // a
+
+// let num1 = 12.000000000345;
+// let num2 = 12.345;
+// console.log(num1.toPrecision(1)); // 1e+1
+// console.log(num1.toPrecision(2)); // 12
+// console.log(num1.toPrecision(3)); // 12.0
+// console.log(num2.toPrecision(3)); // 12.3
+// console.log(num2.toPrecision()); // 12.3
+
+// console.log(Number.isInteger(10));      // true
+// console.log(Number.isInteger(10.0));    // true
+// console.log(Number.isInteger(10.1));    //
+
+// console.log(Number.isSafeInteger(2 ** 53 -1));
+
+// Number.MIN_SAFE_INTEGER
+// Number.MAX_SAFE_INTEGER
+
+// const obj = new Boolean(true);
+// console.log(obj.toLocaleString());
+// console.log(obj.toString());
+
+// let obj = new String('string')
+// console.log(typeof obj);        // object
+
+// let s1 = "hello ";
+// console.log(s1.concat("world"));    // hello world
+// console.log(s1);                    // hello
+
+// let s = "hello";
+// console.log(s.concat(" ", "world", "!"));
+
+// let
+
+// let s = "hello";
+// console.log(s + " " + "world" + "!");
+
+// let s = "12345678";
+// let sub = s.slice();
+// console.log(sub);   // '23'
+
+// let s = "12345678";
+// // let sub = s.substring(1, 4);    // '234'
+// let sub = s.slice(1, 4); // '234'
+// console.log(sub);
+
+// let s = "12345678";
+// let sub = s.substring(1, 3);
+// console.log(sub);
+
+// let s = "12345678";
+// let sub = s.substr(1, 4);
+// console.log(sub);
+
+// let s = "12345678";
+// let sub = s.slice(-10, 5);  // 12345
+// console.log(sub);
+
+// let s = "12345678";
+// let sub = s.substring(1, -6);
+// console.log(sub);
+
+// let s = "12345678";
+// let sub = s.substr(4, -6);
+// console.log(sub); // 56
+
+// let s = "aabcabcd";
+// console.log(s.indexOf("bc"));   // 2
+// console.log(s.lastIndexOf("bc"));   // 5
+
+// let s = "aabcabcdbc";
+// let sub = "bc";
+// let posArr = [];
+// let pos = s.lastIndexOf(sub);
+
+// while (pos !== -1) {
+//   posArr.push(pos);
+//   pos = s.lastIndexOf(sub, pos - 1);
+// }
+
+// console.log(posArr);
+
+
+
