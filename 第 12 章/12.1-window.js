cdea4b490441bb96e142025a397f85fee92004b9
ZhangTuo <13120409722@163.com>
2022-07-06 17:46:07 +0800

    12.1

--- 第 12 章/12.1-window.js
+++ 第 12 章/12.1-window.js
@@ -2,7 +2,7 @@
  * @Author: ZhangTuo 13120409722@163.com
  * @Date: 2022-05-26 15:54:14
  * @LastEditors: ZhangTuo 13120409722@163.com
- * @LastEditTime: 2022-05-26 16:20:13
+ * @LastEditTime: 2022-07-06 17:32:43
  * @FilePath: \study-js\第 12 章\12.1-window.js
  * @Description:
  */
@@ -22,4 +22,46 @@
 // console.log(window.top);
 // console.log(window.parent);
 
+// console.log(window);
+// console.log(window.top);
+// console.log(window.parent);
+// console.log(window.self);
 
+// console.log(window === window.self);
+
+// window.moveTo(0, 0);
+// window.moveBy(0,100)
+
+// window.open('http://www.baidu.com');
+// window.name = 'win-1';
+// console.log(window.name);
+
+// window.open('http://www.baidu.com', 'win-2');
+
+// const win3 = window.open('http://www.baidu.com', 'win-3', 'width=300,height=300,top=100,left=100');
+// setTimeout(()=>{
+//     win3.close();
+// },2000)
+
+// setTimeout('console.log("hello")', 1000);
+// setTimeout(() => {
+//   console.log("hello");
+// }, 1000);
+
+// const id = setTimeout(() => {
+//   console.log("hello");
+// }, 1000);
+
+// clearTimeout(id);
+
+// alert('hello');
+// if (confirm("是否确认")) {
+//   console.log("确认");
+// } else {
+//   console.log("取消");
+// }
+
+// const res = prompt("what is your name?");
+// if (res !== null) {
+//   console.log("hello " + res);
+// }
