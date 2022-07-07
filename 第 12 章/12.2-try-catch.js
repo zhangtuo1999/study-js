b8e0506c03449779153c7f3d8ab89fd103a77d90
ZhangTuo <13120409722@163.com>
2022-07-07 15:04:13 +0800

    12.2 12.3

---
+++ 第 12 章/12.2-try-catch.js
@@ -0,0 +1,23 @@
+// function fn() {
+//   try {
+//     let a
+//     let a
+//   } catch (error) {
+//     return 1;
+//   } finally {
+//     return 2;
+//   }
+// }
+
+// console.log(fn());  // 2
+
+// throw 1
+// throw true
+// throw 'error'
+// throw { a: 1 }
+
+// try{
+//     throw 1
+// }catch(error){
+//     console.log(error);
+// }
---
+++ 第 12 章/12.3-调试.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+  return a + b;
+}
+debugger;
+console.log(sum(1, 2));
